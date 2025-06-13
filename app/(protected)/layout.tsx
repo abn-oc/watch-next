eae88ad3978c85f6711c03204119d13f5fc040cc
abn-oc <abdullah.iht22@gmail.com>
2025-06-13 00:04:07 -0700

    cleanup

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -94,7 +94,7 @@ export default function ProtectedLayout({
             <div className="flex gap-5 items-center">
               <Link 
                 href={"/home"} 
-                className="text-xl font-bold bg-gradient-to-r from-primary to-primary/60 bg-clip-text text-transparent hover:opacity-80 transition-opacity"
+                className="text-xl font-bold bg-gradient-to-r from-purple-600 to-pink-500 bg-clip-text text-transparent hover:opacity-50 transition-opacity"
               >
                 Watch Next
               </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,12 @@ export default async function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Watch Next</Link>
+              <Link 
+                href={"/"} 
+                className=" text-xl font-bold bg-gradient-to-r from-purple-600 to-pink-500 bg-clip-text text-transparent hover:opacity-50 transition-opacity"
+              >
+                Watch Next
+              </Link>
               <div className="flex items-center gap-2">
                 <ThemeSwitcher/>
               </div>
